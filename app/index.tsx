c66870c0002c16eb6ca3d647c6d373568a5279f6
Christian Dagoy <christian.dagoy@hcdc.edu.ph>
2026-02-28 22:17:35 +0800

    Initial commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,44 +1,43 @@
+import { Image, ScrollView, Text, View, KeyboardAvoidingView, TouchableOpacity } from "react-native";
+import SearchBar from "../components/SearchBar"; 
 import { useRouter } from "expo-router";
-import { Button, Image, ScrollView, Text, View } from "react-native";
-import SearchBar from "../components/SearchBar"; // Path matches your folder structure
 
 const Home = () => {
   const router = useRouter();
 
   return (
-    <ScrollView style={{ backgroundColor: '#f8545b' }}>
-      <View style={{ alignItems: 'center' }}>
+    <ScrollView style={{ flex: 1, padding: 20, backgroundColor: '#f8545b' }}>
+      <KeyboardAvoidingView style={{ flex: 1 }} behavior="padding">
         <SearchBar />
-        <Text style={{ 
-          textAlign: 'center', 
-          marginVertical: 20, 
-          fontSize: 32, 
-          fontWeight: 'bold', 
-          color: 'white' 
-        }}>
-          Home Screen
-        </Text>
+        <View style={{ alignItems: 'center' }}>
+          
+          <Text style={{ 
+            textAlign: 'center', 
+            marginVertical: 20, 
+            fontSize: 32, 
+            fontWeight: 'bold', 
+            color: 'white' 
+          }}>
+            Home Screen
+          </Text>
 
-        <Image
-          source={{ uri: 'https://a-static.besthdwallpaper.com/aespa-s-giselle-in-hot-mess-mv-shoot-wallpaper-2560x1600-125742_7.jpg' }}
-          style={{ width: 500, height: 250, marginBottom: 30 }}
-        />
-
-        <View style={{
-          backgroundColor: 'white',
-          borderRadius: 20,
-          padding: 15,
-          width: '10%',
-          marginTop: 10,
-          alignItems: 'center'
-        }}>
-          <Button
-            title="GO TO ORDERS"
-            onPress={() => router.push("/order")}
-            color="#f8545b"
+          <Image
+            source={{ uri: 'https://a-static.besthdwallpaper.com/aespa-s-giselle-in-hot-mess-mv-shoot-wallpaper-2560x1600-125742_7.jpg' }}
+            style={{ width: 500, height: 250, marginBottom: 30 }}
           />
+
+          <TouchableOpacity style={{
+              backgroundColor: 'white',
+              padding: 10,
+              alignItems: 'center',
+              width: '80%',
+              margin: 20}}
+              onPress={() => router.push("/order")} activeOpacity={0.8}>
+              <Text style={{ color: '#f8545b' }}>GO TO ORDERS</Text>
+            </TouchableOpacity>
+
         </View>
-      </View>
+      </KeyboardAvoidingView>
     </ScrollView>
   );
 };
